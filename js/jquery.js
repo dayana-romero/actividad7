45148f5bfa8916593d7f365636f7bfa837154fc5
dayana-romero <jromeroc@ibero.edu.co>
2021-05-20 19:53:19 -0500

    mi primer repositorio

---
+++ js/jquery.js
@@ -0,0 +1,218 @@
+
+$(document).ready (function(){
+    alert("¡¡ BIENVENIDOS !!")
+}
+    )
+
+    $(".lugares1").click(function(){
+        $(this).css("color","brown")
+    })
+
+
+    $(".lugares2").click(function(){
+        $(this).css("color","gray")
+    })
+
+
+    $(".lugares3").click(function(){
+        $(this).css("color","brown")
+    })
+
+
+    $(".lugares4").click(function(){
+        $(this).css("color","gray")
+    })
+
+
+    $(".lugares5").click(function(){
+        $(this).css("color","brown")
+    })
+
+
+    $(".lugares6").click(function(){
+        $(this).css("color","gray")
+    })
+
+
+    $(".lugares7").click(function(){
+        $(this).css("color","brown")
+    })
+
+
+    $(".lugares8").click(function(){
+        $(this).css("color","gray")
+    })
+
+
+    $(".lugares9").click(function(){
+        $(this).css("color","brown")
+    })
+
+
+    $(".lugares10").click(function(){
+        $(this).css("color","gray")
+    })
+
+
+    $(".lugares1").dblclick(function(){
+        $(this).css("background","#F1E580 ")
+    })
+    
+
+    $(".lugares2").dblclick(function(){
+        $(this).css("background","#F1E580 ")
+    })
+    
+
+    $(".lugares3").dblclick(function(){
+        $(this).css("background","#F1E580 ")
+    })
+    
+
+    $(".lugares4").dblclick(function(){
+        $(this).css("background","#F1E580 ")
+    })
+    
+
+    $(".lugares5").dblclick(function(){
+        $(this).css("background","#F1E580 ")
+    })
+    
+
+    $(".lugares6").dblclick(function(){
+        $(this).css("background","#F1E580 ")
+    })
+    
+
+    $(".lugares7").dblclick(function(){
+        $(this).css("background","#F1E580 ")
+    })
+    
+
+    $(".lugares8").dblclick(function(){
+        $(this).css("background","#F1E580 ")
+    })
+    
+
+    $(".lugares9").dblclick(function(){
+        $(this).css("background","#F1E580 ")
+    })
+    
+
+    $(".lugares10").dblclick(function(){
+        $(this).css("background","#F1E580 ")
+    })
+    
+    $(".lugares10").mouseover(function(){
+        $(this).css("font-size"," 30px")
+    })
+    $(".lugares10").mouseout(function(){
+        $(this).css("font-size"," 20px")
+    })
+
+    $(".lugares1").mouseover(function(){
+        $(this).css("font-size"," 30px")
+    })
+    $(".lugares1").mouseout(function(){
+        $(this).css("font-size"," 20px")
+    })
+
+    $(".lugares2").mouseover(function(){
+        $(this).css("font-size"," 30px")
+    })
+    $(".lugares2").mouseout(function(){
+        $(this).css("font-size"," 20px")
+    })
+
+    $(".lugares3").mouseover(function(){
+        $(this).css("font-size"," 30px")
+    })
+    $(".lugares3").mouseout(function(){
+        $(this).css("font-size"," 20px")
+    })
+
+    $(".lugares4").mouseover(function(){
+        $(this).css("font-size"," 30px")
+    })
+    $(".lugares4").mouseout(function(){
+        $(this).css("font-size"," 20px")
+    })
+
+    $(".lugares5").mouseover(function(){
+        $(this).css("font-size"," 30px")
+    })
+    $(".lugares5").mouseout(function(){
+        $(this).css("font-size"," 20px")
+    })
+
+    $(".lugares6").mouseover(function(){
+        $(this).css("font-size"," 30px")
+    })
+    $(".lugares6").mouseout(function(){
+        $(this).css("font-size"," 20px")
+    })
+
+    $(".lugares7").mouseover(function(){
+        $(this).css("font-size"," 30px")
+    })
+    $(".lugares7").mouseout(function(){
+        $(this).css("font-size"," 20px")
+    })
+
+    $(".lugares8").mouseover(function(){
+        $(this).css("font-size"," 30px")
+    })
+    $(".lugares8").mouseout(function(){
+        $(this).css("font-size"," 20px")
+    })
+
+    $(".lugares9").mouseover(function(){
+        $(this).css("font-size"," 30px")
+    })
+    $(".lugares9").mouseout(function(){
+        $(this).css("font-size"," 20px")
+    })
+
+
+
+    $(".parrafo1").mouseover(function(){
+        $(this).css("background"," #F1E580 ")
+        $(this).css("color"," brown ")
+    })
+
+    $(".parrafo1").mouseout(function(){
+        $(this).css("background","#EABCFE ")
+        $(this).css("color","black ")
+    })
+
+
+
+    $(".parrafo2").mouseover(function(){
+        $(this).css("background","#F1E580 ")
+        $(this).css("color"," brown ")
+    })
+
+    $(".parrafo2").mouseout(function(){
+        $(this).css("background","#EABCFE ")
+        $(this).css("color","black ")
+    })
+
+
+
+    $(".parrafo3").mouseover(function(){
+        $(this).css("background","#F1E580 ")
+        $(this).css("color"," brown ")
+    })
+
+    $(".parrafo3").mouseout(function(){
+        $(this).css("background","#EABCFE ")
+        $(this).css("color","black ")
+    })
+
+
+
+    
+
+
+
+    
